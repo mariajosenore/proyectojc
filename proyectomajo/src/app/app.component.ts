75b0cc4c14aa7a0b8806724a4542301fd0538c07
MAB1144-Python <mab1144@hotmail.com>
2024-11-06 00:15:46 -0500

    router aplication

--- proyectomajo/src/app/app.component.ts
+++ proyectomajo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyectomajo';
+  constructor(private router: Router) {}
+
+  goToHome() {
+    this.router.navigate(['/', 'home']);
+  }
+  goToAbout() {
+    this.router.navigate(['/', 'about']);
+  }
+  goToServices() {
+    this.router.navigate(['/', 'services']);
+  }
 }
