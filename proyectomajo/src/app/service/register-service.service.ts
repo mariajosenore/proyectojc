a237b0bd109cbfb3615c156c8a6525d16853cb44
MAB1144-Python <mab1144@hotmail.com>
2024-11-16 21:48:05 -0500

    connect backend

--- proyectomajo/src/app/service/register-service.service.ts
+++ proyectomajo/src/app/service/register-service.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private serverUrl_credit = 'https://bkd-general.getmab.com/';
+  private options = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  register(email: string, firstName: string, lastName: string): Observable<string> {
+    const usuario = {
+
+      email: email,
+      first_name: firstName,
+      last_name: lastName,
+      password: "password",
+    };
+
+    const body = JSON.stringify(usuario);
+
+    return this.http.post<string>(`${this.serverUrl_credit}register_Womenfit/`, body, this.options);
+
+ }
+
 }
